1152ac7fe65a5e5327b48f61668de9745f0b6123
jodnddus <lockhost23@gmail.com>
2023-06-29 20:59:49 +0900

    add problem type

--- intercept-system.js
+++ intercept-system.js
@@ -1,6 +1,7 @@
 /**
  * title: 요격 시스템
  * link: https://school.programmers.co.kr/learn/courses/30/lessons/181188
+ * algorithm: greedy
  */
 
 const targets = [[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]];
